d90fd3741368051b67120a064ae3a142210d31b5
junicodefire <junicodefire@gmail.com>
2020-06-03 22:30:21 +0100

    fix

---
+++ build/precache-manifest.c589f81acd042cb1ed3b71a5400cf6ca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7491149119592f49ccddf75718a9276b",
+    "url": "/MovieDBReact/index.html"
+  },
+  {
+    "revision": "fe1b45bc570880dbaddb",
+    "url": "/MovieDBReact/static/css/main.b95c4bfe.chunk.css"
+  },
+  {
+    "revision": "c1ca2ad993487d38ce70",
+    "url": "/MovieDBReact/static/js/2.cb621d2c.chunk.js"
+  },
+  {
+    "revision": "dd207babaeb913476be46155cc504ef5",
+    "url": "/MovieDBReact/static/js/2.cb621d2c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe1b45bc570880dbaddb",
+    "url": "/MovieDBReact/static/js/main.382cd6e1.chunk.js"
+  },
+  {
+    "revision": "9eb6ea7e0aeb6fddb459",
+    "url": "/MovieDBReact/static/js/runtime-main.c986a7d6.js"
+  }
+]);
